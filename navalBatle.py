e22f26289a81932dd99dc363de5418551c9223ef
seiton35 <prosto99-00@mail.ru>
2020-05-14 16:46:30 +0300

    добавил фоны и расчертил поле

--- navalBatle.py
+++ navalBatle.py
@@ -1,32 +1,37 @@
 import pygame 
 
 pygame.init()
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((600, 800))
 
 pygame.display.set_caption("naval battle")
 
-x = 30
-y = 30
-speed = 10
-
 run = True
+
+bg = pygame.image.load('images/background.png')
+paper = pygame.image.load('images/paper.png')
+
+def drawWindow():
+	win.blit(bg, (0, 0))
+	win.blit(paper, (20, 10))
+
+	pygame.draw.rect(win, (0,0,0), (94,124, 3, 232))
+	pygame.draw.rect(win, (0,0,0), (325,124, 3, 232))
+	pygame.draw.rect(win, (0,0,0), (94,124, 232, 3))
+	pygame.draw.rect(win, (0,0,0), (94,354, 232, 3))
+
+	pygame.draw.rect(win, (0,0,0), (256,469, 3, 232))
+	pygame.draw.rect(win, (0,0,0), (485,469, 3, 232))
+	pygame.draw.rect(win, (0,0,0), (256,469, 232, 3))
+	pygame.draw.rect(win, (0,0,0), (256,700, 232, 3))
+
+	pygame.display.update()
+
+	#цвет фона win.fill((0,0,88))
+
 while run:
 	pygame.time.delay(100)
+	drawWindow()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
-
-	keys = pygame.key.get_pressed()
-	if keys[pygame.K_LEFT]:
-		x -= speed
-	if keys[pygame.K_RIGHT]:
-		x += speed
-	if keys[pygame.K_UP]:
-		y -= speed
-	if keys[pygame.K_DOWN]:
-		y += speed
-
-	#win.fill((0,0,0))
-	pygame.draw.rect(win, (0,0,255), (x, y, 50 ,50))
-	pygame.display.update()
