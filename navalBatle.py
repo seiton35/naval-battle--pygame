cc1da79cb888b661fcd08f117cd7000060ae0f72
seiton35 <prosto99-00@mail.ru>
2020-05-14 13:05:38 +0300

    изменение координат кирпича и отрисовка экрана

--- navalBatle.py
+++ navalBatle.py
@@ -5,6 +5,10 @@ win = pygame.display.set_mode((500,500))
 
 pygame.display.set_caption("naval battle")
 
+x = 30
+y = 30
+speed = 10
+
 run = True
 while run:
 	pygame.time.delay(100)
@@ -13,5 +17,16 @@ while run:
 		if event.type == pygame.QUIT:
 			run = False
 
-	pygame.draw.rect(win, (0,0,255), (30, 40, 50 ,10))
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_LEFT]:
+		x -= speed
+	if keys[pygame.K_RIGHT]:
+		x += speed
+	if keys[pygame.K_UP]:
+		y -= speed
+	if keys[pygame.K_DOWN]:
+		y += speed
+
+	#win.fill((0,0,0))
+	pygame.draw.rect(win, (0,0,255), (x, y, 50 ,50))
 	pygame.display.update()
